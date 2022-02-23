df1ee67f55475a003eaed60be9b6d3b9d64f6759
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-02-23 04:26:15 +0000

    done1

---
+++ p2original.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3)
+{
+  printf("Enter the value of x1,y1,x2 and y2\n");
+  scanf("%f %f %f %f",x1,y1,x2,y2);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+ int a,b,c,;
+  a=sqrt(())
+  
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
+{
+
+}
+int main()
+{
+  
+}
